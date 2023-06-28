36e1800b15b8845765e83447be868921d7f288bf
Kent Kawashima <kentkawashima@gmail.com>
2023-06-28 09:15:11 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='biodatatypes',
+    version='0.1.0',
+    description='Biological sequence datatypes',
+    url='https://github.com/kentwait/biodatatypes',
+    author='Kent Kawashima',
+    author_email='kentkawashima@gmail.com',
+    license='MIT',
+    keywords=['bioinformatics', 'biological sequence', 'datatypes'],
+    packages=find_packages(),
+    install_requires=[],
+    python_require='>=3.8',
+)
