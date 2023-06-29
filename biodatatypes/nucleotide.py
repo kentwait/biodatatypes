671856400fb977fc96db2a84a97e04845eebfccf
Kent Kawashima <kentkawashima@gmail.com>
2023-06-29 06:27:46 +0000

    Fixes incorrect Enum subclassing

--- biodatatypes/nucleotide.py
+++ biodatatypes/nucleotide.py
@@ -12,6 +12,18 @@ class Nucleotide(Enum):
     Mask = 6  # Masked, #
     Special = 7  # Special, @
     Gap = 0  # Gap, -
+    # Degenerate symbols
+    R = 8  # A or G, purine
+    Y = 9 # C or T, pyrimidine
+    S = 10  # G or C, strong
+    W = 11  # A or T, weak
+    K = 12  # G or T, keto
+    M = 13  # A or C, amino
+    B = 14  # C or G or T, not A
+    D = 15  # A or G or T, not C
+    H = 16  # A or C or T, not G
+    V = 17  # A or C or G, not T
+    N = 18  # A or C or G or T, any
 
     def __str__(self) -> str:
         if self.name == 'Gap':
@@ -63,69 +75,9 @@ class Nucleotide(Enum):
     
     def is_special(self):
         return self.is_masked() or self.is_gap()
- 
-    # Properties
     
-    def is_purine(self):
-        return self == Nucleotide.A or self == Nucleotide.G
-
-    def is_pyrimidine(self):
-        return self == Nucleotide.C or self == Nucleotide.T
-
-    def is_strong(self):
-        return self == Nucleotide.G or self == Nucleotide.C
-
-    def is_weak(self):
-        return self == Nucleotide.A or self == Nucleotide.T
-
-    def is_amino(self):
-        return self == Nucleotide.A or self == Nucleotide.C
-
-    def is_keto(self):
-        return self == Nucleotide.G or self == Nucleotide.T
-
-  
-class DegenNucleotide(Nucleotide):
-    # I = 6  # Inosine
-    # X = 7  # Xanthosine
-    # P = 8  # Pseudouridine
-    # Degenerate symbols
-    R = 8  # A or G, purine
-    Y = 9 # C or T, pyrimidine
-    S = 10  # G or C, strong
-    W = 11  # A or T, weak
-    K = 12  # G or T, keto
-    M = 13  # A or C, amino
-    B = 14  # C or G or T, not A
-    D = 15  # A or G or T, not C
-    H = 16  # A or C or T, not G
-    V = 17  # A or C or G, not T
-    N = 18  # A or C or G or T, any
-
-    # Symbol type
-    
-    # def is_standard(self):
-    #     return (
-    #         self == Nucleotide.A or 
-    #         self == Nucleotide.C or 
-    #         self == Nucleotide.G or 
-    #         self == Nucleotide.T or 
-    #         self == Nucleotide.U)   
-
-    # def is_nonstandard(self):
-    #     return (
-    #         self == Nucleotide.I or 
-    #         self == Nucleotide.X or 
-    #         self == Nucleotide.P)
-
     def is_any(self):
         return self == Nucleotide.N
-
-    def is_unknown(self):
-        return self.is_any()
-    
-    def is_other(self):
-        return self.is_any()
    
     def is_ambiguous(self):
         return (
@@ -143,26 +95,27 @@ class DegenNucleotide(Nucleotide):
 
     def is_degenerate(self):
         return self.is_ambiguous
-
+ 
     # Properties
     
     def is_purine(self):
-        return super().is_purine() or self == Nucleotide.R
+        return self == Nucleotide.A or self == Nucleotide.G or self == Nucleotide.R
 
     def is_pyrimidine(self):
-        return super().is_pyrimidine() or self == Nucleotide.Y
+        return self == Nucleotide.C or self == Nucleotide.T or self == Nucleotide.Y
 
     def is_strong(self):
-        return super().is_strong() or self == Nucleotide.S
+        return self == Nucleotide.G or self == Nucleotide.C or self == Nucleotide.S
 
     def is_weak(self):
-        return super().is_weak() or self == Nucleotide.W
+        return self == Nucleotide.A or self == Nucleotide.T or self == Nucleotide.W
 
     def is_amino(self):
-        return super().is_amino() or self == Nucleotide.M
+        return self == Nucleotide.A or self == Nucleotide.C or self == Nucleotide.M
 
     def is_keto(self):
-        return super().is_keto() or self == Nucleotide.K
+        return self == Nucleotide.G or self == Nucleotide.T or self == Nucleotide.K
+
 
 # Constants
 TO_ONE_LETTER_SYMBOL = {
