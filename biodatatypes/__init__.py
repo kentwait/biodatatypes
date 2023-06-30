f7a09f411e3e0d90e2f02b888d86e99f8a60f040
Kent Kawashima <kentkawashima@gmail.com>
2023-06-30 06:12:38 +0000

    Adds sequence types

--- biodatatypes/__init__.py
+++ biodatatypes/__init__.py
@@ -1,2 +1,3 @@
-from biodatatypes.nucleotide import Nucleotide
-from biodatatypes.aminoacid import AminoAcid
+from biodatatypes.nucleotide import Nucleotide, NucleotideSequence
+from biodatatypes.aminoacid import AminoAcid, AminoAcidSequence
+from biodatatypes.codon import Codon, CodonSequence
